518426b8422575c17e8114be5ccbae587221a323
maingate_choi <ttbj123@gmail.com>
2016-05-25 16:29:53 +0900

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+
+urlpatterns = [
+   url(r'^$', 'blog.views.index', name = 'index'),
+   url(r'^(?P<pk>\d+)/$', 'blog.views.post_detail', name='post_detail'),
+]
+
+
---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Post
+
+def index(request):
+	post_list=Post.objects.all()
+	return render(request, 'blog/index.html', {'post_list':post_list,})
+
+def post_detail(request, pk):
+	post=Post.objects.get(pk=pk)
+	return render(request, 'blog/post_detail.html', {'post':post,})
